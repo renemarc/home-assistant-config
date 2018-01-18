1a61fe864193eaa59252187e2dd6fd1e1974bc0e
renemarc <renemarc@gmail.com>
2018-01-18 12:45:27 -0500

    Python components

---
+++ custom_components/light/lightpack.py
@@ -0,0 +1,110 @@
+import logging
+import telnetlib
+
+import voluptuous as vol
+
+# Import the device class from the component that you want to support
+from homeassistant.components.light import Light, PLATFORM_SCHEMA
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_API_KEY
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'lightpack'
+ATTR_NAME = 'profile'
+
+# Validation of the user's configuration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PORT): cv.port,
+    vol.Optional(CONF_API_KEY): cv.string
+})
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    # Assign configuration variables. The configuration check takes care they are present. 
+    host = config.get(CONF_HOST)
+    port = config.get(CONF_PORT)
+    api_key = config.get(CONF_API_KEY)
+
+    # Add device
+    lightpack = Lightpack(host, port, api_key)
+    add_devices([lightpack])
+
+    def set_profile(call):
+        """Instruct the light to change profile."""
+        profile = call.data[ATTR_NAME]
+        lightpack.set_lightpack(True, profile)
+
+    hass.services.register(DOMAIN, set_profile.__name__, set_profile)
+
+class Lightpack(Light):
+    """Representation of a Lightpack"""
+    def __init__(self, host, port, api_key):
+        """Initialize a Lightpack."""
+        self._host = host
+        self._port = port
+        self._api_key = api_key
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the display name of this light."""
+        return 'Lightpack'
+
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend, if any."""
+        return 'mdi:television-guide'
+
+    @property
+    def is_on(self):
+        """Return true if light is on."""
+        try:
+            telnet = telnetlib.Telnet(self._host, self._port)
+            if (self._api_key != None):
+                telnet.write(("apikey:" + self._api_key + "\n").encode('ascii'))
+                telnet.read_until(b"ok\n", timeout=0.2)
+            telnet.write(("getstatus\n").encode('ascii'))
+            self._state = telnet.read_until(b"\n", timeout=0.2).strip().replace(b"status:", b"") == b"on"
+            telnet.close()
+        except IOError as error:
+            _LOGGER.error('Command "%s" failed with exception: %s', command, repr(error))
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Instruct the light to turn on."""
+        self.set_lightpack(True)
+
+    def turn_off(self, **kwargs):
+        """Instruct the light to turn off."""
+        self.set_lightpack(False)
+
+    def set_lightpack(self, enabled, profile = None):
+        try:
+            telnet = telnetlib.Telnet(self._host, self._port)
+            if (self._api_key != None):
+                telnet.write(("apikey:" + self._api_key + "\n").encode('ascii'))
+                telnet.read_until(b"\n", timeout=0.2)
+            telnet.write(("lock\n").encode('ascii'))
+            telnet.read_until(b"\n", timeout=0.2)
+            if (enabled == True):
+                telnet.write(("setstatus:on\n").encode('ascii'))
+                if (profile != None):
+                    telnet.read_until(b"\n", timeout=0.2)
+                    telnet.write(("setprofile:" + profile + "\n").encode('ascii'))
+                    _LOGGER.error('lightpack profile set to : ' + profile)
+            else:
+                telnet.write(("setstatus:off\n").encode('ascii'))
+            telnet.read_until(b"\n", timeout=0.2)
+            telnet.write(("unlock\n").encode('ascii'))
+            telnet.read_until(b"\n", timeout=0.2)
+            telnet.close()
+            self._state = enabled
+        except IOError as error:
+            _LOGGER.error('Command "%s" failed with exception: %s', command, repr(error))
+            self._state = False
+            return None
+            
+    def update(self):
+        """Periodically check to see if the light is on."""
+        self._state = self.is_on
---
+++ custom_components/sensor/doomsday_clock.py
@@ -0,0 +1,185 @@
+"""
+Support for Doomsday Clock from the Bulletin of the Atomic Scientists.
+
+Convert data into parsable time from the Timeline page at
+https://thebulletin.org/timeline
+
+Based on work by Matt Bierner.
+See https://github.com/mattbierner/MinutesToMidnight
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.doomsday_clock/
+"""
+import logging
+import re
+import voluptuous as vol
+from datetime import timedelta
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.components.sensor.rest import RestData
+from homeassistant.const import (
+    ATTR_ATTRIBUTION, CONF_NAME, CONF_ICON, CONF_UNIT_OF_MEASUREMENT,
+    CONF_VALUE_TEMPLATE, STATE_UNKNOWN)
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+import homeassistant.helpers.config_validation as cv
+
+REQUIREMENTS = ['beautifulsoup4==4.6.0']
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_NAME = 'Doomsday Clock'
+DEFAULT_ICON = 'mdi:nuke'
+DEFAULT_UNIT_OF_MEASUREMENT = 'min'
+
+CONF_ATTRIBUTION = "Bulletin of the Atomic Scientists"
+CONF_RESOURCE = 'https://thebulletin.org/timeline'
+CONF_SELECTOR = '#content .view-content .node-title'
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(days=1)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.string,
+    vol.Optional(CONF_UNIT_OF_MEASUREMENT,
+        default=DEFAULT_UNIT_OF_MEASUREMENT): cv.string,
+    vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+})
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the Doomsday Clock sensor."""
+    name = config.get(CONF_NAME)
+    resource = CONF_RESOURCE
+    method = 'GET'
+    selector = CONF_SELECTOR
+    payload = headers = auth = None
+    verify_ssl = False
+    unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
+    icon = config.get(CONF_ICON)
+    value_template = config.get(CONF_VALUE_TEMPLATE)
+    if value_template is not None:
+        value_template.hass = hass
+
+    rest = RestData(method, resource, auth, headers, payload, verify_ssl)
+    rest.update()
+
+    if rest.data is None:
+        _LOGGER.error("Unable to fetch data from %s", resource)
+        return False
+
+    add_devices([
+        DoomsdayClockSensor(rest, name, selector, unit_of_measurement, icon,
+            value_template)
+        ], True)
+
+
+class DoomsdayClockSensor(Entity):
+    """Representation of a Doomsday Clock sensor."""
+
+    def __init__(self, rest, name, selector, unit_of_measurement, icon,
+        value_template):
+        """Initialize a Doomsday Clock sensor."""
+        self.rest = rest
+        self._name = name
+        self._state = STATE_UNKNOWN
+        self._unit_of_measurement = unit_of_measurement
+        self._icon = icon
+        self._value_template = value_template
+        self._selector = selector
+        self._sentence = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit_of_measurement of the sensor."""
+        return self._unit_of_measurement
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return self._icon
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return {
+            ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
+            "minutes_to_midnight": self._sentence,
+        }
+
+    @property
+    def sentence(self):
+        return self._sentence
+
+    def numberStringToInt(self, value):
+        switcher = {
+            "zero": 0,
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+        }
+        return switcher.get(value.lower(), None)
+
+    def sentenceToNumber(self, sentence):
+        pattern = (
+            r'(?:(?P<integer>\d+)'
+            r'|(?P<string>zero|one|two|three|four|five|six|seven|eight|nine)'
+            r'(?P<andhalf>.*?half)'
+            r'|(?P<half>half)) (?:minutes|a minute) to midnight'
+            )
+        result = re.search(pattern, sentence, re.M | re.I)
+
+        if result:
+            countdown = 0
+            # Integer minute
+            if (result.group('integer')):
+                countdown = int(result.group('integer'))
+            # String minute
+            if (result.group('string')):
+                countdown = self.numberStringToInt(result.group('string'))
+            # Half minute
+            if (result.group('andhalf') or result.group('half')):
+                countdown += 0.5
+
+            return countdown
+        else:
+            _LOGGER.error(
+                'Could not find pattern "%s" in sentence "%s"', pattern,
+                sentence)
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Get the latest data from the source and updates the state."""
+        self.rest.update()
+
+        from bs4 import BeautifulSoup
+
+        raw_data = BeautifulSoup(self.rest.data, 'html.parser')
+        _LOGGER.debug(raw_data)
+
+        self._sentence = raw_data.select(self._selector)[0].text.strip()
+        _LOGGER.debug(self._sentence)
+
+        value = self.sentenceToNumber(self._sentence)
+        _LOGGER.debug(value)
+
+        if self._value_template is not None:
+            self._state = self._value_template.render_with_possible_json_value(
+                value, STATE_UNKNOWN)
+        else:
+            self._state = value
