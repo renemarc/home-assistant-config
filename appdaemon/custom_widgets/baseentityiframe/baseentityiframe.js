1c7fe15d044c03449a0207255aa39f80c7f0bd43
renemarc <renemarc@gmail.com>
2018-03-13 17:07:29 -0400

    Embed slideshow image info

--- appdaemon/custom_widgets/baseentityiframe/baseentityiframe.js
+++ appdaemon/custom_widgets/baseentityiframe/baseentityiframe.js
@@ -1,14 +1,54 @@
 function baseentityiframe(widget_id, url, skin, parameters)
 {
-    self = this
+    self = this;
 
     // Initialization
 
     self.parameters = parameters;
 
-    var callbacks = []
+    var callbacks = [];
 
-    var monitored_entities = []
+    var monitored_entities = [];
+
+    var _titles = [];
+
+    // Define dynamic methods.
+    if ("title_entity_list" in self.parameters)
+    {
+        function record_title_value(self, state, idx)
+        {
+            _titles[idx] = state.state;
+        }
+        self.record_title_value = record_title_value;
+
+        self.title_index = 0;
+
+        // Set title entities changes to be recorded.
+        var nameAvailable, nameUpdate, funcAvailable, funcUpdate,
+        title_size = self.parameters.title_entity_list.length;
+
+        for (var i = 0; i < title_size; ++i)
+        {
+            _titles[i] = '';
+            entity_title = self.parameters.title_entity_list[i];
+            if (entity_title.trim() != "") {
+                nameAvailable = "OnTitle" + self.title_index + "StateAvailable";
+                funcAvailable = new Function(
+                     "return function " + nameAvailable + "(self, state){ self.record_title_value(self, state, " + self.title_index + ");}"
+                )();
+                Object.defineProperty(self, nameAvailable, {value: funcAvailable, writable: false});
+
+                nameUpdate = "OnTitle" + self.title_index + "StateUpdate";
+                funcUpdate = new Function(
+                     "return function " + nameUpdate + "(self, state){ self.record_title_value(self, state, " + self.title_index + ");}"
+                )();
+                Object.defineProperty(self, nameUpdate, {value: funcUpdate, writable: false});
+
+                monitored_entities.push({"entity": entity_title, "initial": self[nameAvailable], "update": self[nameUpdate]});
+            }
+            self.title_index = self.title_index + 1;
+        }
+    }
 
     // Call the parent constructor to get things moving
 
@@ -34,9 +74,10 @@ function baseentityiframe(widget_id, url, skin, parameters)
         {
             self.set_field(self, "frame_src", self.parameters.url_list[self.index]);
             self.set_field(self, "img_src", "/images/Blank.gif");
+            self.set_field(self, "title", _titles[self.index]);
             size = self.parameters.url_list.length
         }
-       else if ("img_list" in self.parameters)
+        else if ("img_list" in self.parameters)
         {
             var url = self.parameters.img_list[self.index];
             var separator = url.indexOf('?') > -1 ? '&' : '?';
@@ -44,6 +85,7 @@ function baseentityiframe(widget_id, url, skin, parameters)
             var timestamp = Math.floor(date/1000) - (self.cache ? Math.floor(date/1000) % self.cache : 0);
             url = url + separator + "time=" + timestamp;
             self.set_field(self, "img_src", url);
+            self.set_field(self, "title", _titles[self.index]);
             size = self.parameters.img_list.length
         }
         else if ("entity_picture" in self.parameters)
