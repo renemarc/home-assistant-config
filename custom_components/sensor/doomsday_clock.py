91b4b9bffaa4b99d9dcf6dd87d6c0b2eb7baa167
renemarc <renemarc@gmail.com>
2018-07-19 23:15:29 -0400

    Upgrade Doomsday Clock sensor to 1.0.0

--- custom_components/sensor/doomsday_clock.py
+++ custom_components/sensor/doomsday_clock.py
@@ -2,13 +2,13 @@
 Support for Doomsday Clock from the Bulletin of the Atomic Scientists.
 
 Convert data into parsable time from the Timeline page at
-https://thebulletin.org/timeline
+https://thebulletin.org/doomsday-clock/past-announcements/
 
-Based on work by Matt Bierner.
+Based on prior work by Matt Bierner.
 See https://github.com/mattbierner/MinutesToMidnight
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.doomsday_clock/
+https://github.com/renemarc/home_assistant_doomsday_clock/
 """
 import datetime
 import logging
@@ -28,15 +28,15 @@ REQUIREMENTS = ['beautifulsoup4==4.6.0']
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Doomsday Clock'
+DEFAULT_NAME = "Doomsday Clock"
 DEFAULT_ICON = 'mdi:nuke'
 DEFAULT_UNIT_OF_MEASUREMENT = 'min'
 
-CONF_ATTRIBUTION = "Threat assessment by The Bulletin of the Atomic Scientists"
-CONF_RESOURCE = 'https://thebulletin.org/timeline'
-CONF_SELECTOR = '#content .view-content .node-title'
+CONF_ATTRIBUTION = "Threat assessment by the Bulletin of the Atomic Scientists"
+CONF_RESOURCE = 'https://thebulletin.org/doomsday-clock/past-announcements/'
+CONF_SELECTOR = '.uabb-infobox-title'
 
-MIN_TIME_BETWEEN_UPDATES = datetime.timedelta(hours=1)
+MIN_TIME_BETWEEN_UPDATES = datetime.timedelta(hours=6)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -57,6 +57,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
     icon = config.get(CONF_ICON)
     value_template = config.get(CONF_VALUE_TEMPLATE)
+
     if value_template is not None:
         value_template.hass = hass
 
@@ -64,14 +65,13 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     rest.update()
 
     if rest.data is None:
-        _LOGGER.error("Unable to fetch data from %s", resource)
+        _LOGGER.error("Unable to fetch URL: %s", resource)
         return False
 
     add_devices([
         DoomsdayClockSensor(rest, name, selector, unit_of_measurement, icon,
             value_template)
         ], True)
-
 
 class DoomsdayClockSensor(Entity):
     """Representation of a Doomsday Clock sensor."""
@@ -81,12 +81,12 @@ class DoomsdayClockSensor(Entity):
         """Initialize a Doomsday Clock sensor."""
         self.rest = rest
         self._name = name
-        self._state = STATE_UNKNOWN
         self._unit_of_measurement = unit_of_measurement
         self._icon = icon
         self._value_template = value_template
         self._selector = selector
         self._sentence = None
+        self._state = STATE_UNKNOWN
 
     @property
     def name(self):
@@ -113,8 +113,8 @@ class DoomsdayClockSensor(Entity):
         """Return the state attributes."""
         return {
             ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
-            "countdown": self._sentence,
-            "time": self.numberToTime(),
+            'countdown': self._sentence,
+            'time': self.numberToTime(),
         }
 
     @property
@@ -122,55 +122,60 @@ class DoomsdayClockSensor(Entity):
         return self._sentence
 
     def numberStringToInt(self, value):
+        """Convert textual numbers into integers."""
         switcher = {
-            "zero": 0,
-            "one": 1,
-            "two": 2,
-            "three": 3,
-            "four": 4,
-            "five": 5,
-            "six": 6,
-            "seven": 7,
-            "eight": 8,
-            "nine": 9,
+            'eight': 8,
+            'five': 5,
+            'four': 4,
+            'nine': 9,
+            'one': 1,
+            'seven': 7,
+            'six': 6,
+            'three': 3,
+            'two': 2,
+            'zero': 0,
         }
+
         return switcher.get(value.lower(), None)
 
     def sentenceToNumber(self, sentence):
+        """Convert Doomsday Clock string to a number of minutes to midnight."""
         pattern = (
-            r'(?:(?P<integer>\d+)'
-            r'|(?P<string>zero|one|two|three|four|five|six|seven|eight|nine)'
-            r'(?P<andhalf>.*?half)'
-            r'|(?P<half>half)) (?:minutes|a minute) to midnight'
+            r"(?:(?P<integer>\d+)"
+            r"|(?P<string>zero|one|two|three|four|five|six|seven|eight|nine)"
+            r"(?P<andhalf>.*?half)"
+            r"|(?P<half>half)) (?:minutes|a minute) to midnight"
             )
         result = re.search(pattern, sentence, re.M | re.I)
 
-        if result:
-            countdown = 0
-            # Integer minute
-            if (result.group('integer')):
-                countdown = int(result.group('integer'))
-            # String minute
-            if (result.group('string')):
-                countdown = self.numberStringToInt(result.group('string'))
-            # Half minute
-            if (result.group('andhalf') or result.group('half')):
-                countdown += 0.5
-
-            return countdown
-        else:
+        if result is None:
             _LOGGER.error(
-                'Could not find pattern "%s" in sentence "%s"', pattern,
-                sentence)
+                "Could not find any regex pattern in sentence: %s", sentence)
+            return None
+
+        countdown = 0
+        # Integer minute.
+        if (result.group('integer')):
+            countdown = int(result.group('integer'))
+        # String minute.
+        if (result.group('string')):
+            countdown = self.numberStringToInt(result.group('string'))
+        # Half minute.
+        if (result.group('andhalf') or result.group('half')):
+            countdown += 0.5
+
+        return countdown
 
     def numberToTime(self):
-        midnight = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days=1), datetime.time(0, 0, 0, 0))
-        minutes = self._state // 1
-        seconds = (self._state - minutes) * 60
-        delta = datetime.timedelta(minutes=minutes, seconds=seconds)
+        """Convert a number of minutes to midnight into 24h format."""
+        if self._state is not STATE_UNKNOWN:
+            midnight = datetime.datetime.combine(datetime.date.today() +
+                datetime.timedelta(days=1), datetime.time(0, 0, 0, 0))
+            minutes = self._state // 1
+            seconds = (self._state - minutes) * 60
+            delta = datetime.timedelta(minutes=minutes, seconds=seconds)
 
-        return (midnight - delta).strftime('%H:%M:%S')
-
+            return (midnight - delta).strftime('%H:%M:%S')
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
@@ -179,17 +184,36 @@ class DoomsdayClockSensor(Entity):
 
         from bs4 import BeautifulSoup
 
+        # Convert HTML into parse tree.
         raw_data = BeautifulSoup(self.rest.data, 'html.parser')
-        _LOGGER.debug(raw_data)
 
-        self._sentence = raw_data.select(self._selector)[0].text.strip()
-        _LOGGER.debug(self._sentence)
+        # Find the first match, which is the current Doomsday Clock value.
+        try:
+            sentence = raw_data.select(self._selector)[0].text.strip()
+            if not sentence:
+                raise ValueError('Sentence is empty')
+            self._sentence = sentence
+            _LOGGER.debug("Sentence found: %s", self._sentence)
+        except IndexError:
+            _LOGGER.error(
+                "No sentence found using selector: %s. Did the design change?",
+                self._selector)
+            return False
+        except ValueError:
+            _LOGGER.error(
+                "Empty sentence found using selector %s. Did the design change?"
+                , self._selector)
+            return False
 
+        # Convert the Doomsday Clock string into number of minutes to midnight.
         value = self.sentenceToNumber(self._sentence)
-        _LOGGER.debug(value)
+        _LOGGER.debug("Numeric value: %s", value)
 
-        if self._value_template is not None:
-            self._state = self._value_template.render_with_possible_json_value(
-                value, STATE_UNKNOWN)
-        else:
-            self._state = value
+        if value is not None:
+            # Optionally parse a custom template.
+            if self._value_template is not None:
+                self._state = \
+                    self._value_template.render_with_possible_json_value(value,
+                    STATE_UNKNOWN)
+            else:
+                self._state = value
