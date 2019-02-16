b62e90b7188b268781f122dd527dcbbbea63ec33
renemarc <renemarc@gmail.com>
2019-02-16 01:51:10 -0500

    Add index and custom SQL handling

--- custom_components/sensor/gtfs.py
+++ custom_components/sensor/gtfs.py
@@ -4,10 +4,10 @@ Support for GTFS (Google/General Transport Format Schema).
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.gtfs/
 """
+import asyncio
 import os
 import logging
 import datetime
-import threading
 
 import voluptuous as vol
 
@@ -20,6 +20,7 @@ REQUIREMENTS = ['pygtfs==0.1.5']
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_CUSTOM_SQL = 'custom_sql'
 CONF_DATA = 'data'
 CONF_DEPARTURES = 'departures'
 CONF_DESTINATION = 'destination'
@@ -57,6 +58,7 @@ TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 UNIT_OF_MEASUREMENT = 'min'
 DATA_KEY = 'gtfs'
 TRIP_KEY_FORMAT = '{}::{}'
+SQL_RESULT_PER_DAY = 1440
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ORIGIN): cv.string,
@@ -68,19 +70,23 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME): cv.string,
     vol.Optional(CONF_OFFSET, default=0): cv.time_period,
     vol.Optional(CONF_TOMORROW, default=False): cv.boolean,
+    vol.Optional(CONF_CUSTOM_SQL):
+        vol.All(cv.ensure_list),
 })
 
 
-def get_next_departure(sched, start_station_id, end_station_id, offset, 
+async def async_get_next_departure(sched, start_station_id, end_station_id, offset, 
                        position, hass, include_tomorrow=False):
     """Get the next departure for the given schedule."""
     origin_station = sched.stops_by_id(start_station_id)[0]
     destination_station = sched.stops_by_id(end_station_id)[0]
 
     now = datetime.datetime.now() + offset
+    # now = datetime.datetime.strptime("2019-02-14 23:45:00", TIME_FORMAT)
     now_date = now.strftime(DATE_FORMAT)
     trip_key = TRIP_KEY_FORMAT.format(start_station_id, end_station_id)
 
+    # Load timetable from cache or fetch a new one.
     if trip_key in hass.data[DATA_KEY] \
             and now_date in hass.data[DATA_KEY][trip_key]['timetable']:
         timetable = hass.data[DATA_KEY][trip_key]['timetable'][now_date]
@@ -89,19 +95,21 @@ def get_next_departure(sched, start_station_id, end_station_id, offset,
         _LOGGER.info("Refreshing timetable cache for %s.", now_date)
 
         from sqlalchemy.sql import text
+        # import time
+        # time.sleep(5)
 
+        # Fetch all departures for yesterday, today and optionally tomorrow,
+        # up to an overkill maximum in case of a departure every minute for
+        # those days.
         yesterday = now - datetime.timedelta(days=1)
         yesterday_date = yesterday.strftime(DATE_FORMAT)
         tomorrow = now + datetime.timedelta(days=1)
         tomorrow_date = tomorrow.strftime(DATE_FORMAT)
 
-        # Fetch all departures for yesterday, today and optionally tomorrow,
-        # up to an overkill maximum in case of a departure every minute for
-        # those days.
-        limit = 24 * 60 * 60 * 2
+        limit = SQL_RESULT_PER_DAY * 2
         tomorrow_select = tomorrow_where = tomorrow_order = ''
         if include_tomorrow:
-            limit = limit / 2 * 3
+            limit = SQL_RESULT_PER_DAY * 3
             tomorrow_name = tomorrow.strftime('%A').lower()
             tomorrow_select = "calendar.{} AS tomorrow,".format(tomorrow_name)
             tomorrow_where = "OR calendar.{} = 1".format(tomorrow_name)
@@ -174,9 +182,13 @@ def get_next_departure(sched, start_station_id, end_station_id, offset,
                 if yesterday_first is None:
                     yesterday_first = row['origin_departure_date']
                 if yesterday_first != row['origin_departure_date']:
+                    day = {
+                        'day': 'yesterday',
+                        'origin_departure_date': now_date,
+                    }
                     idx = '{} {}'.format(now_date,
                                          row['origin_depart_time'])
-                    timetable[idx] = {**row, **{'day': 'yesterday'}}
+                    timetable[idx] = {**row, **day}
             if row['today'] == 1:
                 if today_first is None:
                     today_first = row['origin_departure_date']
@@ -184,20 +196,27 @@ def get_next_departure(sched, start_station_id, end_station_id, offset,
                     idx_prefix = now_date
                 else:
                     idx_prefix = tomorrow_date
+                day = {
+                    'day': 'today',
+                    'origin_departure_date': idx_prefix,
+                }
                 idx = '{} {}'.format(idx_prefix, row['origin_depart_time'])
-                timetable[idx] = {**row, **{'day': 'today'}}
+                timetable[idx] = {**row, **day}
             if 'tomorrow' in row and row['tomorrow'] == 1 \
                     and tomorrow_date <= row['end_date']:
                 if tomorrow_first is None:
                     tomorrow_first = row['origin_departure_date']
                 if tomorrow_first == row['origin_departure_date']:
+                    day = {
+                        'day': 'tomorrow',
+                        'origin_departure_date': tomorrow_date,
+                    }
                     idx = '{} {}'.format(tomorrow_date,
                                          row['origin_depart_time'])
-                    timetable[idx] = {**row, **{'day': 'tomorrow'}}
+                    timetable[idx] = {**row, **day}
         timetable_updated = True
 
-    # Find the next departure in timetable, reducing its cache footprint if
-    # possible
+    # Find the next departure in timetable and trim the cache size if possible
     item = {}
     skip = next_day = 0
     for key in sorted(timetable.keys()):
@@ -209,9 +228,6 @@ def get_next_departure(sched, start_station_id, end_station_id, offset,
 
         if skip == position:
             item = timetable[key]
-            _LOGGER.debug("Departure %d found for station %s @ %s -> %s",
-                          position, start_station_id, key,
-                          item)
         skip += 1
 
         # Trim tomorrow's useless departures, if any
@@ -230,18 +246,23 @@ def get_next_departure(sched, start_station_id, end_station_id, offset,
     if item == {}:
         return None
 
-    _LOGGER.debug("Departure %d found for station %s @ %s -> %s",
+    _LOGGER.debug("Departure #%d found for station %s @ %s -> %s",
                   position, start_station_id, item['origin_depart_time'], item)
 
-    origin_arrival = now
-    if item['dest_arrival_time'] < item['origin_depart_time']:
+    # Format arrival and departure dates and times, accounting for the
+    # possibility of times crossing over midnight.
+    item_date = datetime.datetime.strptime(item['origin_departure_date'],
+                                           DATE_FORMAT)
+    origin_depart_time = '{} {}'.format(item_date.strftime(DATE_FORMAT),
+                                        item['origin_depart_time'])
+
+    origin_arrival = item_date
+    if item['origin_depart_time'] < item['origin_arrival_time']:
         origin_arrival -= datetime.timedelta(days=1)
     origin_arrival_time = '{} {}'.format(origin_arrival.strftime(DATE_FORMAT),
                                          item['origin_arrival_time'])
 
-    origin_depart_time = '{} {}'.format(now_date, item['origin_depart_time'])
-
-    dest_arrival = now
+    dest_arrival = item_date
     if item['dest_arrival_time'] < item['origin_depart_time']:
         dest_arrival += datetime.timedelta(days=1)
     dest_arrival_time = '{} {}'.format(dest_arrival.strftime(DATE_FORMAT),
@@ -257,6 +278,7 @@ def get_next_departure(sched, start_station_id, end_station_id, offset,
     arrival_time = datetime.datetime.strptime(dest_arrival_time, TIME_FORMAT)
 
     seconds_until = (depart_time - datetime.datetime.now()).total_seconds()
+    # seconds_until = (depart_time - datetime.datetime.strptime("2019-02-14 23:45:00", TIME_FORMAT)).total_seconds()
     minutes_until = int(seconds_until / 60)
 
     route = sched.routes_by_id(item['route_id'])[0]
@@ -309,6 +331,7 @@ async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
     offset = config.get(CONF_OFFSET)
     include_tomorrow = config.get(CONF_TOMORROW)
+    custom_sql = config.get(CONF_CUSTOM_SQL)
 
     if DATA_KEY not in hass.data:
         hass.data[DATA_KEY] = dict()
@@ -335,20 +358,44 @@ async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     joined_path = os.path.join(gtfs_dir, sqlite_file)
     gtfs = pygtfs.Schedule(joined_path)
 
+    # Create database file if it doesn't exist
     # pylint: disable=no-member
     if not gtfs.feeds:
+        _LOGGER.info("Creating database for \"%s\", this may take some time.",
+                     data)
         pygtfs.append_feed(gtfs, os.path.join(gtfs_dir, data))
+
+        import sqlalchemy
+
+        # Run any custom SQL queries
+        for sql_query in custom_sql:
+            gtfs.engine.execute(sqlalchemy.sql.text(sql_query))
+
+        # Create additional indexes to speed-up lookups
+        meta = sqlalchemy.MetaData()
+        meta.reflect(bind=gtfs.engine)
+        sqlalchemy.Index('ix_trips_service_id', meta.tables[
+            'trips'].columns.get('service_id')).create(bind=gtfs.engine)
+        sqlalchemy.Index('ix_stop_times_trip_id', meta.tables[
+            'stop_times'].columns.get('trip_id')).create(bind=gtfs.engine)
+        sqlalchemy.Index('ix_stop_times_stop_id', meta.tables[
+            'stop_times'].columns.get('stop_id')).create(bind=gtfs.engine)
+        sqlalchemy.Index('ix_stop_times_stop_sequence', meta.tables[
+            'stop_times'].columns.get('stop_sequence')).create(
+            bind=gtfs.engine)
+
+        _LOGGER.info("Database \"%s.sqlite\" creation complete.", data)
 
     sensors = []
     for position in departures:
         sensors.append(GTFSDepartureSensor(pygtfs=gtfs,
-                            name=name,
-                            origin=origin,
-                            destination=destination,
-                            offset=offset,
-                            position=position,
-                            mode=mode,
-                            include_tomorrow=include_tomorrow))
+                                           name=name, 
+                                           origin=origin,
+                                           destination=destination,
+                                           offset=offset,
+                                           position=position,
+                                           mode=mode,
+                                           include_tomorrow=include_tomorrow))
     add_entities(sensors, True)
 
 
@@ -371,8 +418,7 @@ class GTFSDepartureSensor(Entity):
         self._unit_of_measurement = UNIT_OF_MEASUREMENT
         self._state = None
         self._attributes = {}
-        self.lock = threading.Lock()
-        # await self.async_update()
+        self.lock = asyncio.Lock()
 
     @property
     def name(self):
@@ -403,8 +449,8 @@ class GTFSDepartureSensor(Entity):
 
     async def async_update(self):
         """Get the latest data from GTFS and update the states."""
-        with self.lock:
-            self._departure = get_next_departure(
+        async with self.lock:
+            self._departure = await async_get_next_departure(
                 self._pygtfs, self.origin, self.destination, self._offset,
                 self.position, self.hass, self._include_tomorrow)
             if not self._departure:
